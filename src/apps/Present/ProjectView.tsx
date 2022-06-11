39cc538fd5ac174e340d10c0f7755caf6d98d18b
Ethan Horowitz <ethan.horowitz@duke.edu>
2022-06-11 15:54:59 -0400

    new project navigation

--- src/apps/Present/ProjectView.tsx
+++ src/apps/Present/ProjectView.tsx
@@ -56,7 +56,7 @@ const ProjectView = ( { filePath, width, setPage }: AppProps ) => {
     }
 
     return (
-        <div className='pb-12' style={{fontFamily: 'Quicksand', backgroundColor: '#242A3E', minHeight: '100%'}}>
+        <div className='pb-12' style={{fontFamily: 'Avenir', backgroundColor: '#242A3E', minHeight: '100%'}}>
             {
                 viewProject === '' ?
             <>
@@ -64,16 +64,25 @@ const ProjectView = ( { filePath, width, setPage }: AppProps ) => {
                 <h1 className='text-center text-white font-bold text-3xl pt-6 font-bold pb-12'>{file.name}</h1>
 
                 {file && (
-                <div className='px-8'>
+                <div className='px-8 flex flex-wrap justify-center'>
                     {Object.keys(file.projects).map((name, ind) => {
                         let proj = file.projects[name];
                         return (
-                            <div key={ind} className={'proj-list flex items-center text-white' + (width < 600 ? ' column' : '')} onClick={() => setViewProject(name)}>
-                                <img src={proj.images.length > 0 ? proj.images[0] : ''} alt="" className='w-56 h-56 object-cover object-center shadow-lg shadow-gray-600 border-2 border-gray-200 hover:scale-105 transition-transform duration-400 ease rounded-md' />
-                                <div className='w-1/2'>
+                            // <div key={ind} className={'proj-list flex items-center text-white' + (width < 600 ? ' column' : '')} onClick={() => setViewProject(name)}>
+                            //     <img src={proj.images.length > 0 ? proj.images[0] : ''} alt="" className='w-56 h-56 object-cover object-center shadow-lg shadow-gray-600 border-2 border-gray-200 hover:scale-105 transition-transform duration-400 ease rounded-md' />
+                            //     <div className='w-1/2'>
+                            //         <span className='text-2xl font-semibold mb-4 block'>{name}</span>
+                            //         <span>{proj.shortDesc}</span>
+                            //     </div>
+                            // </div>
+                            <div key={ind} className='proj-item relative flex flex-col items-center text-white m-8 hover:scale-110 transition-transform duration-400 ease w-64' onClick={() => setViewProject(name)}>
+                                <img src={proj.images.length > 0 ? proj.images[0] : ''} alt="" className='w-64 h-64 object-cover object-center shadow-lg shadow-gray-600 border-2 border-gray-200 rounded-md' />
+                                <span className='absolute top-0 left-0 text-gray-900 w-full font-bold bg-[rgba(255,255,255,0.8)] text-center p-2 rounded-t-md'>{name}</span>
+                                <span className='proj-sdesc text-left p-3 absolute bottom-0 left-0 w-full text-gray-900 bg-white rounded-b-md text-sm'>{proj.shortDesc}</span>
+                                {/* <div className='w-1/2'>
                                     <span className='text-2xl font-semibold mb-4 block'>{name}</span>
                                     <span>{proj.shortDesc}</span>
-                                </div>
+                                </div> */}
                             </div>
                         )
                     })}
@@ -81,8 +90,10 @@ const ProjectView = ( { filePath, width, setPage }: AppProps ) => {
                 )}
             </>
             : <>
-                <span onClick={() => setViewProject("")} className='text-white pt-4 pl-4 mb-[-2rem] text-2xl block cursor-pointer hover:text-gray-400'>Back</span>
-                <h1 className={'text-center text-white font-bold text-3xl pt-6 font-bold' + (file.projects[viewProject].url ? ' pb-4' : ' pb-12')}>{file.projects[viewProject].name}</h1>
+                <span onClick={() => setViewProject("")} className='text-white pt-4 pl-4 mb-[-1rem] text-2xl block cursor-pointer hover:text-gray-400'>Back</span>
+                <div className='flex justify-center'>
+                    <h1 style={{maxWidth: '80%'}} className={'text-center text-white font-bold text-3xl pt-6 font-bold' + (file.projects[viewProject].url ? ' pb-4' : ' pb-12')}>{file.projects[viewProject].name}</h1>
+                </div>
                 {
                     file.projects[viewProject].url && <span className='text-center block text-white text-xl pb-8'>Check it out <a href={file.projects[viewProject].url} target='_blank' className='text-orange-400'>here</a></span>
                 }
